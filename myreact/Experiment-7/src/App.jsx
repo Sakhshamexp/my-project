22bf83988d372a19f6620b2a5cf9a73c4a5e0456
Sakhshamexe <23BAI70580@cuchd.in>
2025-09-14 21:55:56 +0530

    p push

--- myreact/Experiment-7/src/App.jsx
+++ myreact/Experiment-7/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import ProductsList from './productcard.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Experiment-0 </h1>
+    <ProductsList />
   )
 }
 
