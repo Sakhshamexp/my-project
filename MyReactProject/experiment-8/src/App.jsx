49d9287475394b35f273bcc680e3db06d90b78c3
Sakhshamexe <23BAI70580@cuchd.in>
2025-09-16 12:11:22 +0530

    Add Experiment-8

---
+++ MyReactProject/experiment-8/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Library from "./components/LibraryManager";
+
+function App() {
+  return (
+    <div className="App">
+      <Library />
+    </div>
+  );
+}
+
+export default App;
